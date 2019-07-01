8d03729cc75f0b3bacf00e10515082e7b5c78675
Ory <212671846@logon.ds.ge.com>
2019-07-01 15:14:51 +0200

    add project-view-navbar

---
+++ src/app/core/dashboard/project-view/project-view-navbar/project-view-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-view-navbar',
+  templateUrl: './project-view-navbar.component.html',
+  styleUrls: ['./project-view-navbar.component.css']
+})
+export class ProjectViewNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
