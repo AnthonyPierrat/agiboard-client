75e736d950632244edde0d7b3732c32ffb23eb68
adavid1 <axeldvi.pro@gmail.com>
2019-07-02 08:41:35 +0200

    navbar working

--- src/app/core/dashboard/project-view/project-view-navbar/project-view-navbar.component.ts
+++ src/app/core/dashboard/project-view/project-view-navbar/project-view-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-project-view-navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectViewNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
+
+  idProject: number;
 
   ngOnInit() {
+    this.idProject = Number.parseInt(this.route.snapshot.paramMap.get("projectId"));
   }
-
 }
